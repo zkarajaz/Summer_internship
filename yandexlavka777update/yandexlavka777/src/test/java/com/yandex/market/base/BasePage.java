e7a4ed3f73ae5432c687bdd8cdbb97537d7b9bf7
Igor Rautio <144445184+zkarajaz@users.noreply.github.com>
2025-07-06 06:25:57 +0300

    Add files via upload

---
+++ yandexlavka777update/yandexlavka777/src/test/java/com/yandex/market/base/BasePage.java
@@ -0,0 +1,17 @@
+package com.yandex.market.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    protected BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ yandexlavka777update/yandexlavka777/src/test/java/com/yandex/market/pages/ProductPage.java
@@ -0,0 +1,36 @@
+package com.yandex.market.pages;
+
+import com.yandex.market.base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class ProductPage extends BasePage {
+    @FindBy(xpath = "//*[@data-apiary-widget-name='@light/AddToCartButton']")
+    private WebElement addToCartButton;
+
+
+    @FindBy(xpath = "//*[@id=\"basketOutline\"]")
+    private WebElement cartLink;
+
+    @FindBy(xpath = "//*[@id=\"basketOutline\"]")
+    private WebElement cartIcon;
+    public ProductPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickAddToCartButton() {
+        addToCartButton.click();
+    }
+
+    public CartPage goToCart() {
+        wait.until(ExpectedConditions.elementToBeClickable(cartLink)).click();
+        return new CartPage(driver);
+    }
+
+    // Добавляем публичный метод для ожидания обновления иконки
+    public void waitForCartIconUpdate(String expectedText) {
+        wait.until(ExpectedConditions.textToBePresentInElement(cartIcon, expectedText));
+    }
+}
